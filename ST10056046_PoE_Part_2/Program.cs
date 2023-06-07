702095c67ffc7ea93a45712b84762ff3accc69cb
Widske <Widske@DESKTOP-P06RT3O>
2023-06-07 18:26:59 +0200

    Add project files.

---
+++ ST10056046_PoE_Part_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ST10056046_PoE_Part_2
+{
+
+}
